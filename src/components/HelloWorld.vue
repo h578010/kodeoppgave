67310b6c4993345cbef97dc8e732084aaa992bb8
Synnøve Sørensen <578010@stud.hvl.no>
2020-10-31 00:28:32 +0100

    Ferdig post-req

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,22 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="6">
+        <v-text-field label="Laanebeløp: " v-model = "laanebelop" ></v-text-field>
+        <v-text-field label="Rente: "></v-text-field>
+      </v-col>
+      <v-col cols="6">
+        <v-text-field label="Startdato: "></v-text-field>
+        <v-text-field label="Innfrielsesdato: "></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col align="center">
+        <v-label>{{heading}}</v-label><br>
+        <v-btn color="base" type="submit" @click="makePostRequest()">Press me</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -35,24 +24,48 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  private the_head = "Synne";
+  private laanebelop = "2000000";
+  private nominellRente = 3;
+  private terminGebyr = 30; 
+  private utlopsDato = "2045-01-01";
+  private saldoDato = "2020-01-01";
+  private datoForsteInnbetaling = "2020-02-01";
+  private ukjentVerdi = "TERMINBELOP";
+
+  get heading() {
+    return this.the_head;
+  }
+
+  setHead() {
+    this.the_head = "Steinar";
+  }
+
+  async makePostRequest() {
+
+    let req = {
+      laanebelop: this.laanebelop,
+      nominellRente: this.nominellRente,
+      terminGebyr: this.terminGebyr,
+      utlopsDato: this.utlopsDato,
+      saldoDato: this.saldoDato,
+      datoForsteInnbetaling: this.datoForsteInnbetaling,
+      ukjentVerdi: this.ukjentVerdi
+    }
+
+    let res = await fetch("https://visningsrom.stacc.com/dd_server_laaneberegning/rest/laaneberegning/v1/nedbetalingsplan", {
+      method: "POST",
+      headers: {
+				'Content-Type': 'application/json'
+			},
+      body: JSON.stringify(req)
+    });
+    if (res.ok) {
+      let resObject = await res.json();
+      console.log(resObject);
+
+    }
+    
+    }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
